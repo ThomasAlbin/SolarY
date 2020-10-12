be0d3b42ad5d6a90edae618836bde39a83b28ac4
ThomasAlbin <ThomasAlbin@users.noreply.github.com>
2020-10-12 22:35:21 +0200

    Update test_general_photometry.py

--- solary/tests/test_general_photometry.py
+++ solary/tests/test_general_photometry.py
@@ -1,1 +1,19 @@
-import configparserimport pytestimport solarydef test_appmag2irr():        config = configparser.ConfigParser()    config.read('_config/constants.ini')    appmag_irr_i0 = float(config['photometry']['appmag_irr_i0'])    irradiance1 = solary.general.photometry.appmag2irr(app_mag=0)    assert pytest.approx(irradiance1) == appmag_irr_i0    irradiance2 = solary.general.photometry.appmag2irr(app_mag=-5)    assert pytest.approx(irradiance2) == appmag_irr_i0 * 100.0    irradiance3 = solary.general.photometry.appmag2irr(app_mag=1)    assert pytest.approx(irradiance3) == 1.0024422165005002e-08
+import configparser
+
+import pytest
+import solary
+
+def test_appmag2irr():
+
+    config = configparser.ConfigParser()
+    config.read('_config/constants.ini')
+    appmag_irr_i0 = float(config['photometry']['appmag_irr_i0'])
+
+    irradiance1 = solary.general.photometry.appmag2irr(app_mag=0)
+    assert pytest.approx(irradiance1) == appmag_irr_i0
+
+    irradiance2 = solary.general.photometry.appmag2irr(app_mag=-5)
+    assert pytest.approx(irradiance2) == appmag_irr_i0 * 100.0
+
+    irradiance3 = solary.general.photometry.appmag2irr(app_mag=1)
+    assert pytest.approx(irradiance3) == 1.0024422165005002e-08

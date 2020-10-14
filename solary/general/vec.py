6245678fa1a81b5c5075c6743cdf37d5d5f0ec09
ThomasAlbin <ThomasAlbin@users.noreply.github.com>
2020-10-14 10:52:23 +0200

    Create vec.py

---
+++ solary/general/vec.py
@@ -0,0 +1,1 @@
+# Import standard librariesimport mathdef norm(vector):    # Compute the norm    norm_res = math.sqrt(sum(abs(elem)**2.0 for elem in vector))    return norm_resdef unify(vector):        vector_norm = norm(vector)        unit_vector = [vector_elem / vector_norm for vector_elem in vector]        return unit_vector

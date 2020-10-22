962db0741243e5fcec14f1672dbc4d8c9ab7455c
ThomasAlbin <ThomasAlbin@users.noreply.github.com>
2020-10-22 17:09:25 +0200

    Update test_neo_data.py

--- solary/tests/test_neo_data.py
+++ solary/tests/test_neo_data.py
@@ -8,8 +8,13 @@ def test__get_neodys_neo_nr():
 
 def test_download():
     
-    dl_status = solary.neo.data.download()
+    dl_status, _ = solary.neo.data.download()
     assert dl_status == 'OK'
+
+    dl_status, row_exp = solary.neo.data.download(row_exp=True)
+    assert dl_status == 'OK'
+    assert type(row_exp) == int
+    assert row_exp >= 0
     
 # def test_neo_sqlite_db():
     

7362ec5f859fb0594f41faa4d4f863398fbd4162
ThomasAlbin <ThomasAlbin@users.noreply.github.com>
2020-10-27 00:57:45 +0100

    Update test_neo_data.py

--- solary/tests/test_neo_data.py
+++ solary/tests/test_neo_data.py
@@ -7,33 +7,35 @@ import solary
 #     assert type(neo_nr) == int
 #     assert neo_nr >= 0
 
-def test_download():
+# def test_download():
     
-    dl_status, _ = solary.neo.data.download()
-    assert dl_status == 'OK'
+#     dl_status, _ = solary.neo.data.download()
+#     assert dl_status == 'OK'
 
-    dl_status, _ = solary.neo.data.download(download_path='tests/_temp')
-    assert dl_status == 'OK'
+#     dl_status, _ = solary.neo.data.download(download_path='tests/_temp')
+#     assert dl_status == 'OK'
 
-    dl_status, row_exp = solary.neo.data.download(row_exp=True)
-    assert dl_status == 'OK'
-    assert type(row_exp) == int
-    assert row_exp >= 0
+#     dl_status, row_exp = solary.neo.data.download(row_exp=True)
+#     assert dl_status == 'OK'
+#     assert type(row_exp) == int
+#     assert row_exp >= 0
     
 def test_read():
     
     neo_dict_data = solary.neo.data.read()
     assert neo_dict_data[0]['Name'] == '433'
-    assert pytest.approx(neo_dict_data[0]['Sem-Maj_Axis_[AU]'], abs=1e-2) == 1.46
-    assert pytest.approx(neo_dict_data[0]['Ecc_[]'], abs=1e-2) == 0.22
+    assert pytest.approx(neo_dict_data[0]['SemMajAxis_AU'], abs=1e-2) == 1.46
+    assert pytest.approx(neo_dict_data[0]['Ecc_'], abs=1e-2) == 0.22
 
     neo_dict_data = solary.neo.data.read(path='tests/_temp')
     assert neo_dict_data[0]['Name'] == '433'
-    assert pytest.approx(neo_dict_data[0]['Sem-Maj_Axis_[AU]'], abs=1e-2) == 1.46
-    assert pytest.approx(neo_dict_data[0]['Ecc_[]'], abs=1e-2) == 0.22
+    assert pytest.approx(neo_dict_data[0]['SemMajAxis_AU'], abs=1e-2) == 1.46
+    assert pytest.approx(neo_dict_data[0]['Ecc_'], abs=1e-2) == 0.22
 
-# def test_neo_sqlite_db():
+def test_neo_sqlite_db():
     
-#     neo_sqlite = solary.neo.data.neodys_database()
+    neo_sqlite = solary.neo.data.neodys_database(new=True)
     
-#     neo_sqlite.close()
+    neo_sqlite.create()
+    
+    neo_sqlite.close()

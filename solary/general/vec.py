12178c118ee4581b35ade3a872521ec965ab0873
ThomasAlbin <ThomasAlbin@users.noreply.github.com>
2020-10-29 00:11:24 +0100

    Update vec.py

--- solary/general/vec.py
+++ solary/general/vec.py
@@ -1,11 +1,13 @@
 """
 vec.py
 
-Auxiliary function for vector computations
+Auxiliary functions for vector computations.
+
 """
 
 # Import standard libraries
 import math
+
 
 def norm(vector):
     """
@@ -22,12 +24,20 @@ def norm(vector):
     norm_res : float
         Norm of the input vector.
 
+    Examples
+    --------
+    >>> import solary
+    >>> vec_norm = solary.general.vec.norm(vector=[3.0, 5.0, -5.9])
+    >>> vec_norm
+    8.295179322956196
+
     """
 
     # Compute the norm by summing all squared elements
     norm_res = math.sqrt(sum(abs(elem)**2.0 for elem in vector))
 
     return norm_res
+
 
 def unify(vector):
     """
@@ -44,6 +54,19 @@ def unify(vector):
     unit_vector : list
         Unified / Normalised vector.
 
+    Examples
+    --------
+    >>> import solary
+    >>> unit_vec = solary.general.vec.unify(vector=[1.0, 5.0, 10.0])
+    >>> unit_vec
+    [0.0890870806374748, 0.44543540318737396, 0.8908708063747479]
+
+    Now check the norm of the resulting vector
+
+    >>> vec_norm = solary.general.vec.norm(vector=unit_vec)
+    >>> vec_norm
+    1.0
+
     """
 
     # Compute the norm of the input vector
@@ -53,6 +76,7 @@ def unify(vector):
     unit_vector = [vector_elem / vector_norm for vector_elem in vector]
 
     return unit_vector
+
 
 def dot_prod(vector1, vector2):
     """
@@ -70,12 +94,22 @@ def dot_prod(vector1, vector2):
     dotp_res : float
         Dot product of both vectors.
 
+    Examples
+    --------
+    >>> import solary
+    >>> dot_product_res = solary.general.vec.dot_prod(vector1=[1.5, -4.0, 8.0], \
+                                                      vector2=[-5.0, -4.20, 0.0])
+
+    >>> dot_product_res
+    9.3
+
     """
 
     # Compute dot product
     dotp_res = sum(v1_i * v2_i for v1_i, v2_i in zip(vector1, vector2))
 
     return dotp_res
+
 
 def phase_angle(vector1, vector2):
     """
@@ -96,6 +130,16 @@ def phase_angle(vector1, vector2):
     angle_rad : float
         Phase angle in radians between vector1 and vector2.
 
+    Examples
+    --------
+    >>> import math
+    >>> import solary
+    >>> ph_angle_rad = solary.general.vec.phase_angle(vector1=[1.0, 0.0], \
+                                                      vector2=[1.0, 1.0])
+    >>> ph_angle_deg = math.degrees(ph_angle_rad)
+    >>> ph_angle_deg
+    45.0
+
     """
 
     # Compute the phase angle by considering the rearranged, known geometric definition of the dot
@@ -104,6 +148,7 @@ def phase_angle(vector1, vector2):
                           / (norm(vector1) * norm(vector2)))
 
     return angle_rad
+
 
 def substract(vector1, vector2):
     """
@@ -123,6 +168,14 @@ def substract(vector1, vector2):
     diff_vector : list
         Difference vector with the same dimensionality as vector1.
 
+    Examples
+    --------
+    >>> import solary
+    >>> vector_diff = solary.general.vec.substract(vector1=[1.0, 4.0, 2.0], \
+                                                   vector2=[-8.0, 0.0, 1.0])
+    >>> vector_diff
+    [9.0, 4.0, 1.0]
+
     """
 
     # Set an empty list for the vector difference / substraction
@@ -137,6 +190,7 @@ def substract(vector1, vector2):
 
     return diff_vector
 
+
 def inverse(vector):
     """
     Inverse the vector's elements. Alternatively, apply -1 on a Numpy array.
@@ -150,6 +204,13 @@ def inverse(vector):
     -------
     inv_vector : TYPE
         Inverse output vector with the same dimensionality as vector.
+
+    Examples
+    --------
+    >>> import solary
+    >>> inverse_vector = solary.general.vec.inverse(vector=[1.0, 2.0, -3.0])
+    >>> inverse_vector
+    [-1.0, -2.0, 3.0]
 
     """
 

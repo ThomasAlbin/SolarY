4acaf7527da7b88e5e8216fcf6f6142cd33856cc
ThomasAlbin <tho.albin@gmail.com>
2020-12-12 13:30:37 +0100

    [CI/CD] Deployment Testing

--- solary/__init__.py
+++ solary/__init__.py
@@ -1,6 +1,10 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+__project__ = "SolarY"
+__author__ = "Dr.-Ing. Thomas Albin"
+__version__ = "ON_GIT_RELEASE"
+
 from . import _config
 
 from . import tests

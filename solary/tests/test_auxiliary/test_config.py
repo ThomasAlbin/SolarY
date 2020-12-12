aea7761eb3fdb96ee632692a842148a0f8edf23b
ThomasAlbin <tho.albin@gmail.com>
2020-12-12 20:18:41 +0100

    [Tests] PEP8 + NumpyDoc

--- solary/tests/test_auxiliary/test_config.py
+++ solary/tests/test_auxiliary/test_config.py
@@ -1,17 +1,47 @@
+"""
+test_config.py
+
+Testing suite for solary/auxiliary/config.py
+
+"""
+
+# Import solary
 import solary
 
+
 def test_get_constants():
-    
+    """
+    Test function to check whether the constants file is read successfully
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Call the constants get function
     constant_config = solary.auxiliary.config.get_constants()
-    
+
+    # If the reading was successful the config object shall have miscellaneous sections and
+    # corresponding values. One of them is called "constants"
     constant_config_sections = constant_config.sections()
-    
     assert 'constants' in constant_config_sections
 
+
 def test_get_paths():
-    
+    """
+    Test function to check whether the path config file is read.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Call the paths config file
     paths_config = solary.auxiliary.config.get_paths()
-    
+
+    # If the reading was successful the config object shall have miscellaneous sections and
+    # corresponding values. One of them is called "neo"
     paths_config_sections = paths_config.sections()
-    
     assert 'neo' in paths_config_sections

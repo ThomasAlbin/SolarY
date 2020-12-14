0d07c4cc2dce9617bac19b180b1e1dcc46cfe44b
ThomasAlbin <tho.albin@gmail.com>
2020-12-14 18:17:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import setuptools
 
 import solary
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='solary',
     version=solary.__version__,
@@ -13,9 +10,12 @@ setuptools.setup(
     package_data={'solary': ['_config/constants.ini', '_config/paths.ini']},
     include_package_data=True,
     description="A Space Science library for asteroid, comets and meteors",
+    long_description="A Space Science library for asteroid, comets and meteors",
     url="https://github.com/ThomasAlbin/SolarY",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research"
     ],
     python_requires='>=3.8',
     install_requires=[

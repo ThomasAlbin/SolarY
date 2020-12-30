9e0ccf8d4798d821a17cad0833a1b0ff0ba8d0c6
Thomas Albin <tho.albin@gmail.com>
2020-12-30 23:42:25 +0100

    [general] Added fwhm 2 std computation

--- solary/general/geometry.py
+++ solary/general/geometry.py
@@ -29,3 +29,25 @@ def circle_area(radius):
     area = math.pi * (radius ** 2.0)
 
     return area
+
+
+def fwhm2std(fwhm):
+    """
+    Convert the Full Width at Half Maximum to the corresponding Gaussian standard deviation.
+
+    Parameters
+    ----------
+    fwhm : float
+        Full Width at Half Maximum.
+
+    Returns
+    -------
+    gauss_sigma : float
+        Standard deviation assuming a Gaussian distribution.
+
+    """
+
+    # Compute the standard deviation
+    gauss_sigma = fwhm / (2.0 * math.sqrt(2.0 * math.log(2)))
+
+    return gauss_sigma
--- solary/tests/test_general/test_geometry.py
+++ solary/tests/test_general/test_geometry.py
@@ -29,3 +29,20 @@ def test_circle_area():
     # Second example
     circle_area_res1 = solary.general.geometry.circle_area(radius=2.0)
     assert circle_area_res1 == math.pi * 4.0
+
+def test_fwhm2std():
+    """
+    Testing the FWHM to standard deviation computation
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Compute the standard deviation corresponding FWHM and vice versa
+    sigma1_exp = 5.0
+    fwhm1 = 2.0 * sigma1_exp * math.sqrt(2.0 * math.log(2))
+
+    sigma1_res = solary.general.geometry.fwhm2std(fwhm1)
+    assert sigma1_res == sigma1_exp

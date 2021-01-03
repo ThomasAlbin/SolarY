b5b880c5109674f7505095fe590952b273df6a7f
Thomas Albin <tho.albin@gmail.com>
2021-01-03 22:10:47 +0100

    [instruments] PEP8 + Docstring

--- solary/tests/test_instruments/test_camera.py
+++ solary/tests/test_instruments/test_camera.py
@@ -1,10 +1,9 @@
 """
-TBD
+test_camera.py
+
+Testing suite for solary/instruments/camera.py
 
 """
-
-# Import standard libraries
-import math
 
 # Import installed libraries
 import pytest
@@ -13,43 +12,86 @@ import pytest
 import solary
 
 
-@pytest.fixture
-def ccd_test_config():
-    
+# Define a test fixture that is being used in all tests. The fixture loads configuration files.
+@pytest.fixture(name='ccd_test_config')
+def fixture_ccd_test_config():
+    """
+    Fixture to load the test configuration files.
+
+    Returns
+    -------
+    test_ccd_dict : dict
+        CCD test configuration.
+
+    """
+
+    # Get the test config file paths
     test_paths_config = solary.auxiliary.config.get_paths(test=True)
 
+    # Get the path to the CCD config file
     test_ccd_path = \
         solary.auxiliary.parse.get_test_file_path(
             test_paths_config['instruments_camera_ccd']['properties'])
 
-    test_ccd = solary.instruments.camera.read_ccd_config(test_ccd_path)
+    # Read and parse the CCD config file and return a dictionary with the properties
+    test_ccd_dict = solary.instruments.camera.read_ccd_config(test_ccd_path)
 
-    return test_ccd
+    return test_ccd_dict
+
 
 def test_read_ccd_config(ccd_test_config):
-    
+    """
+    Testing if the config file reading was successful.
+
+    Parameters
+    ----------
+    ccd_test_config : dict
+        CCD config dictionary.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Check if the fixture load is a dictionary
+    assert isinstance(ccd_test_config, dict)
+
+    # Check the pixels
     assert ccd_test_config['pixels'] == [4096, 4112]
 
-def test_Optical(ccd_test_config):
-    
-    test_ccd = solary.instruments.camera.CCD(ccd_test_config)
-    
-    assert test_ccd.pixels == ccd_test_config['pixels']
-    assert test_ccd.pixel_size == ccd_test_config['pixel_size']
-    assert test_ccd.dark_noise == ccd_test_config['dark_noise']
-    assert test_ccd.readout_noise == ccd_test_config['readout_noise']
-    assert test_ccd.full_well == ccd_test_config['full_well']
-    assert test_ccd.quantum_eff == ccd_test_config['quantum_eff']
+def test_ccd(ccd_test_config):
+    """
+    Testing the CCD Class.
 
-    # test now the chip size
-    assert test_ccd.chip_size[0] == \
-        ccd_test_config['pixels'][0] * ccd_test_config['pixel_size'] / 1000.0
-    
-    # test now the pixel size in m**2
-    assert test_ccd.pixel_size_sq_m == (ccd_test_config['pixel_size'] ** 2.0) * 10.0 ** (-12.0)
-    
-#     assert test_telescope.sec_mirror_area == math.pi \
-#         * ((telescope_test_optics['sec_mirror_dia'] / 2.0) ** 2.0)
-        
-#     assert test_telescope.collect_area == \
-#         test_telescope.main_mirror_area - test_telescope.sec_mirror_area
+    Parameters
+    ----------
+    ccd_test_config : dict
+        CCD config dictionary.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # Initiate the CCD class
+    test_ccd_class = solary.instruments.camera.CCD(ccd_test_config)
+
+    # Check the config depending attributes
+    assert test_ccd_class.pixels == ccd_test_config['pixels']
+    assert test_ccd_class.pixel_size == ccd_test_config['pixel_size']
+    assert test_ccd_class.dark_noise == ccd_test_config['dark_noise']
+    assert test_ccd_class.readout_noise == ccd_test_config['readout_noise']
+    assert test_ccd_class.full_well == ccd_test_config['full_well']
+    assert test_ccd_class.quantum_eff == ccd_test_config['quantum_eff']
+
+    # Check the first entry of the chip size. Multiply the number of pixels (x dimension) times the
+    # pixel size in micro meters. Finally divide by 1000 to get a result in mm.
+    assert test_ccd_class.chip_size[0] == ccd_test_config['pixels'][0] \
+                                          * ccd_test_config['pixel_size'] / 1000.0
+
+    # Check the size of a single pixel in m^2. The pixel size squared leads to micro meter squared.
+    # Divide the results by 10^-12.
+    assert test_ccd_class.pixel_size_sq_m == (ccd_test_config['pixel_size'] ** 2.0) \
+                                             * 10.0 ** (-12.0)

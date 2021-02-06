36ea36db4204f80d386e880a4b69eacb6a4b3d7d
ThomasAlbin <tho.albin@gmail.com>
2021-02-06 18:17:48 +0100

    Update download.py

--- solary/auxiliary/download.py
+++ solary/auxiliary/download.py
@@ -6,10 +6,8 @@ This sub-module contains auxiliary functions to download miscellaneous datasets.
 """
 
 # Import standard libraries
-import glob
 import urllib.parse
 import urllib.request
-import os
 
 # Import solary
 from . import config
@@ -18,30 +16,39 @@ from . import parse
 # Get the file paths
 GENERIC_KERNEL_CONFIG = config.get_spice_kernels(ktype='generic')
 
+
 def spice_generic_kernels():
     """
-    TBW.
+    Function to download the generic SPICE kernels into the solary data storage directory:
+    ~HOME/solary_data/.
+
+    Parameters
+    ----------
+    None.
+
+    Returns
+    -------
 
     """
 
-    # Set a placeholder list for the resulting MD5 values
+    # Set a placeholder list for the filepaths and corresponding / resulting MD5 values
     kernel_hashes = {}
 
-
+    # Iterate through the SPICE config file. Each section corresponds to an individual SPICE kernel
     for kernel in GENERIC_KERNEL_CONFIG.sections():
-        
-        download_filename = \
-            parse.setnget_file_path(GENERIC_KERNEL_CONFIG[kernel]['dir'], \
-                                                     GENERIC_KERNEL_CONFIG[kernel]['file'])
 
-        # Downlaod the file and store it in the kernels directory
+        # Set the download filepath
+        download_filename = parse.setnget_file_path(GENERIC_KERNEL_CONFIG[kernel]['dir'],
+                                                    GENERIC_KERNEL_CONFIG[kernel]['file'])
+
+        # Download the file and store it in the kernels directory
         downl_file_path, _ = urllib.request.urlretrieve(url=GENERIC_KERNEL_CONFIG[kernel]['url'], \
                                                         filename=download_filename)
 
         # Compute the MD5 hash value
         md5_hash = parse.comp_md5(downl_file_path)
 
-        # Append kernel name and corresponding hash value to the list
+        # Append the filepath and MD5 in the dictionary
         kernel_hashes[download_filename] = md5_hash
 
     return kernel_hashes

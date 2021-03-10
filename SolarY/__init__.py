8c0929e7085ce0bffafd70c32e170dc39f065ed3
hans smit <Hans.Smit@esa.int>
2021-03-10 19:09:24 +0100

    +ADD: added versioneer support.

--- SolarY/__init__.py
+++ SolarY/__init__.py
@@ -1,7 +1,10 @@
 """TBW."""
 # flake8: noqa
 from . import asteroid, auxiliary, general, instruments, neo
+from ._version import get_versions
 
+__version__ = get_versions()["version"]
+__date__ = get_versions()["date"]
 __project__ = "SolarY"
 __author__ = "Dr.-Ing. Thomas Albin"
-__version__ = "deploy"
+# __version__ = "deploy"

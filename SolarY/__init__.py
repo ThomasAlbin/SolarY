eb66c395ceb0d6df54518005c49cf3ff921235b5
hans smit <Hans.Smit@esa.int>
2021-03-10 19:22:47 +0100

    +FIX: pylint / mypy fixes.

--- SolarY/__init__.py
+++ SolarY/__init__.py
@@ -3,8 +3,8 @@
 from . import asteroid, auxiliary, general, instruments, neo
 from ._version import get_versions
 
-__version__ = get_versions()["version"]
-__date__ = get_versions()["date"]
+__version__ = get_versions()["version"]  # type: ignore
+__date__ = get_versions()["date"]  # type: ignore
 __project__ = "SolarY"
 __author__ = "Dr.-Ing. Thomas Albin"
 # __version__ = "deploy"

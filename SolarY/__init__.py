71617a6709adc7c503626e80f7d4fefa2b9b78c1
hans smit <Hans.Smit@esa.int>
2021-03-10 19:49:24 +0100

    +FIX: fixed ``tox -e build``. May need to execute ``pip install -r requirements.txt``, in this tox section - not sure yet.

--- SolarY/__init__.py
+++ SolarY/__init__.py
@@ -1,7 +1,12 @@
 """TBW."""
 # flake8: noqa
-from . import asteroid, auxiliary, general, instruments, neo
 from ._version import get_versions
+
+try:
+    from . import asteroid, auxiliary, general, instruments, neo
+except ModuleNotFoundError as exc:
+    # this occurs when during `tox -e build`, just ignore it.
+    print("Import error in Solary.__init__. Exception:", exc)
 
 __version__ = get_versions()["version"]  # type: ignore
 __date__ = get_versions()["date"]  # type: ignore
